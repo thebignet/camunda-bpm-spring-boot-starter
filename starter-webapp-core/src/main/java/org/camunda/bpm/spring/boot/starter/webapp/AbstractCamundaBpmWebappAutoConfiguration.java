3479f2ae10f85f2b24c0ccd737b834ddf3a658cf
Jean Detoeuf <jean@upcoding.fr>
2019-05-05 10:26:40 +0200

    feat(spring-boot): added auto-configuration of webapps for spring Webflux
    * separated auto-configuration for MVC from auto-configuration from Webflux

--- starter-webapp-core/src/main/java/org/camunda/bpm/spring/boot/starter/webapp/AbstractCamundaBpmWebappAutoConfiguration.java
+++ starter-webapp-core/src/main/java/org/camunda/bpm/spring/boot/starter/webapp/AbstractCamundaBpmWebappAutoConfiguration.java
@@ -16,31 +16,21 @@
  */
 package org.camunda.bpm.spring.boot.starter.webapp;
 
-import org.camunda.bpm.spring.boot.starter.CamundaBpmAutoConfiguration;
 import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.camunda.bpm.spring.boot.starter.webapp.filter.LazyDelegateFilter.InitHook;
 import org.camunda.bpm.spring.boot.starter.webapp.filter.LazyInitRegistration;
 import org.camunda.bpm.spring.boot.starter.webapp.filter.ResourceLoaderDependingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@ConditionalOnWebApplication
-@AutoConfigureAfter(CamundaBpmAutoConfiguration.class)
-public class CamundaBpmWebappAutoConfiguration implements WebMvcConfigurer {
+public abstract class AbstractCamundaBpmWebappAutoConfiguration {
 
   @Autowired
   private ResourceLoader resourceLoader;
 
   @Autowired
-  private CamundaBpmProperties properties;
+  protected CamundaBpmProperties properties;
 
 
   @Bean
@@ -59,21 +49,6 @@ public class CamundaBpmWebappAutoConfiguration implements WebMvcConfigurer {
   @Bean
   public LazyInitRegistration lazyInitRegistration() {
     return new LazyInitRegistration();
-  }
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    final String classpath = "classpath:" + properties.getWebapp().getWebjarClasspath();
-    registry.addResourceHandler("/lib/**").addResourceLocations(classpath + "/lib/");
-    registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
-    registry.addResourceHandler("/app/**").addResourceLocations(classpath + "/app/");
-  }
-
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-    if (properties.getWebapp().isIndexRedirectEnabled()) {
-      registry.addRedirectViewController("/", "/app/");
-    }
   }
 
 }
